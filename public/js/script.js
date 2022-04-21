b3a8b361ca8edb8befb60fd2c1962a14225fc118
KGabard <kev.gabard@gmail.com>
2022-04-21 16:14:01 +0200

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,26 @@
+const adviceNumber = document.querySelector('.card__header > span');
+const cardQuote = document.querySelector('.card__quote');
+const cardBtn = document.querySelector('.card__btn');
+
+function askForRandomQuote() {
+    fetch('https://api.adviceslip.com/advice')
+    .then(function(res) {
+        if (res.ok) {
+            return res.json();
+        }
+    })
+    .then(function(value) {
+        adviceNumber.innerHTML = value.slip.id;
+        cardQuote.innerHTML = 'Loading...';
+        setTimeout(() => {
+            cardQuote.innerHTML = `“${value.slip.advice}”`;
+        }, 400);
+    })
+    .catch(function(err) {
+        // Une erreur est survenue
+    });
+}
+
+cardBtn.addEventListener('click', askForRandomQuote);
+
+askForRandomQuote();
